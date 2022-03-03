2d3712c27cb00a7d89ad1312ee2ce1f1f4f29b08
Carlos Mohedano <krlosmohedano@gmail.com>
2022-03-03 00:23:37 -0600

    gh-pages deploy

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       <h1>Ikiti Store</h1>
+       <h1>Ikiti Store Mx</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
